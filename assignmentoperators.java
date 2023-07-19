791c79c5a774b99d782369b5d5efc21784733502
AKSHAYKUNDARAGI <133511331+AKSHAYKUNDARAGI@users.noreply.github.com>
2023-07-19 19:00:25 +0530

    ASSIGNMENTS OPERATORS

---
+++ assignmentoperators.java
@@ -0,0 +1,25 @@
+public class assignmentoperators {
+    public static void main(String[] args) {
+        int number = 10;
+
+        //adding
+        number += 2;
+        System.out.println("After addition: num = " + number);
+
+        // subtracting
+        number -= 4;
+        System.out.println("After subtraction: num = " + number);
+
+        // multiplying
+        number *= 7;
+        System.out.println("After multiplication: num = " + number);
+
+        // dividing
+        number /= 2;
+        System.out.println("After division: num = " + number);
+
+        // After modulus
+        number %= 5;
+        System.out.println("After modulus: num = " + number);
+    }
+}
