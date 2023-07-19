53d7272e0cb39786a72bf2bcb58e0d0eed1f0232
AKSHAYKUNDARAGI <133511331+AKSHAYKUNDARAGI@users.noreply.github.com>
2023-07-19 18:54:09 +0530

    relational operators

---
+++ reltionaloper.java
@@ -0,0 +1,18 @@
+public class reltionaloper {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+
+        // num1=num2
+        System.out.println("Equal: " + (num1 == num2));
+
+        // num1!=num2
+        System.out.println("Not Equal: " + (num1 != num2));
+
+        // NUMBER1  Greater Than NUMBER2
+        System.out.println("num1 is Greater Than num2: " + (num1 > num2));
+
+        // NUMBER 2 IS GREATER THAN NUMBER1
+        System.out.println("num1 is Less Than num2: " + (num1 < num2));
+    }
+}
